8d680f176e947ece5ffeccdc5721e3d26666f7fc
AliEm <aliemamhadi@gmail.com>
2020-12-24 16:04:49 +0200

    fix a logger config

--- config/logger.php
+++ config/logger.php
@@ -24,26 +24,32 @@
 
 return [
     'mlog' => [
-//        This is a path to save log file in the format of {logger_name}_{Y-m-d}.log
-        'path' => LOG_DIR,
+//        This is where you choose your stream type, it could be any string in format of available stream listed:
+//        https://www.php.net/manual/en/wrappers.php
+        'path' => 'file://'.LOG_DIR.'/mlog.log',
+
+//        This should be either 'line' or 'json', indicates the logging format.
+//        In case of linear: "[$type][$tracker][$tag][$ip][$route] $message"
+//        In case of json: {"type": $type, "timestamp": $date, "message": "[$tracker][$tag][$ip][$route] $message"}
+        'type' => 'line',
 
 //        This variable can be used when you want to just log a maximum char in each logger call.
 //        if it was null, logger will log complete message.
         'max_length' => null,
 
 //        This variable can be used when you want to add more data to all messages.
-//        At the end each massage will be in this format:
-//        "[$time][$type] [$tag][$ip][$route] $prefix$message"
+//        You can use setLoggerTracker method to overwrite it, but be aware in case of use php preload
+//        restart server.
+//        If $tracker was provided by developer. developer can track that string in logs.
+        'tracker' => 'TT',
+
+//        This variable can be used when you want to add more data to all messages.
+//        You can use setLoggerTracker method to overwrite it, but be aware in case of use php preload
+//        restart server.
 //        If $tag was provided by developer. developer can track that string in logs.
         'tag' => 'NT',
 
 //        This is Phalcon\Logger log level constant
-        'level' => \Phalcon\Logger::DEBUG
-    ],
-    'test' => [
-        'path' => LOG_DIR,
-        'max_length' => null,
-        'tag' => 'TEST',
         'level' => \Phalcon\Logger::DEBUG
     ]
 ];
