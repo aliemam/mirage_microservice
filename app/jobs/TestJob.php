a2531db8dc44f1766a1a9cdba4c71ed28f650f60
AliEm <aliemamhadi@gmail.com>
2021-01-14 12:07:56 +0200

    add TestJob.php

--- app/jobs/TestJob.php
+++ app/jobs/TestJob.php
@@ -22,27 +22,14 @@
  * @author Ali Emamhadi <aliemamhadi@gmail.com>
  */
 
-return [
-    // All namespaces should be defined here to access from other parts of the app.
-    'Namespaces' => [
-        // app
-        'App' => APP_DIR,
-        'App\Constants' => APP_DIR . '/constants/',
-        'App\Controllers' => APP_DIR . '/controllers/',
-        'App\Interfaces' => APP_DIR . '/interfaces/',
-        'App\Jobs' => APP_DIR . '/jobs/',
-        'App\Libs' => APP_DIR . '/libs/',
-        'App\Middlewares' => APP_DIR . '/middleware/',
-        'App\Models' => APP_DIR . '/models/',
-        'App\Queues' => APP_DIR . '/queues/',
-        'App\Routes' => APP_DIR . '/routes/',
-        'App\Traits' => APP_DIR . '/traits/',
-        'Bootstrap' => MIRAGE_APP_DIR . '/bootstrap',
-    ],
+namespace App\Jobs;
 
-    // All the PHP files under each directory which is defined here can be accessed from other parts of the app.
-    'Dirs' => [],
+use Mirage\Libs\L;
 
-    // All the PHP files which are defined here can be accessed from other parts of the app.
-    'Files' => []
-];
+class TestJob extends \Mirage\App\Job
+{
+    public function task($data): array
+    {
+        return [];
+    }
+}
